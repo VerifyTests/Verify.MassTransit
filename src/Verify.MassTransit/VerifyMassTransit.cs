b7305b2507fd22d6f08121c2ad6f7fc13a1b0f44
Simon Cropp <simon.cropp@gmail.com>
2022-06-02 09:10:11 +1000

    Update VerifyMassTransit.cs

--- src/Verify.MassTransit/VerifyMassTransit.cs
+++ src/Verify.MassTransit/VerifyMassTransit.cs
@@ -3,25 +3,22 @@
 public static class VerifyMassTransit
 {
     public static void Enable() =>
-        VerifierSettings.ModifySerialization(settings =>
+        VerifierSettings.AddExtraSettings(serializerSettings =>
         {
-            settings.AddExtraSettings(serializerSettings =>
-            {
-                var converters = serializerSettings.Converters;
-                converters.Add(new ConsumerTestHarnessConverter());
-                converters.Add(new TestHarnessConverter());
-                converters.Add(new ReceivedMessageConverter());
-                converters.Add(new ReceivedMessageListConverter());
-                converters.Add(new SentMessageConverter());
-                converters.Add(new SentMessageListConverter());
-                converters.Add(new PublishedMessageConverter());
-                converters.Add(new PublishedMessageListConverter());
-                converters.Add(new SagaTestHarnessConverter());
-                converters.Add(new SagaListConverter());
-                converters.Add(new MessageContextConverter());
-                converters.Add(new SendContextConverter());
-                converters.Add(new ReceiveContextConverter());
-            });
+            var converters = serializerSettings.Converters;
+            converters.Add(new ConsumerTestHarnessConverter());
+            converters.Add(new TestHarnessConverter());
+            converters.Add(new ReceivedMessageConverter());
+            converters.Add(new ReceivedMessageListConverter());
+            converters.Add(new SentMessageConverter());
+            converters.Add(new SentMessageListConverter());
+            converters.Add(new PublishedMessageConverter());
+            converters.Add(new PublishedMessageListConverter());
+            converters.Add(new SagaTestHarnessConverter());
+            converters.Add(new SagaListConverter());
+            converters.Add(new MessageContextConverter());
+            converters.Add(new SendContextConverter());
+            converters.Add(new ReceiveContextConverter());
         });
 
     internal static bool CanConvertToGeneric(this Type from, Type to)
