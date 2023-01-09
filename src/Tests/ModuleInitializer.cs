6794cc640240ae736753a49af4bd809b3c1cc258
Simon <simon.cropp@gmail.com>
2023-01-09 12:46:31 +1100

    docs

--- src/Tests/ModuleInitializer.cs
+++ src/Tests/ModuleInitializer.cs
@@ -3,12 +3,12 @@
     #region enable
 
     [ModuleInitializer]
-    public static void Initialize()
-    {
+    public static void Initialize() =>
         VerifyMassTransit.Enable();
 
-        #endregion
+    #endregion
 
+    [ModuleInitializer]
+    public static void InitializeOther() =>
         VerifyDiffPlex.Initialize();
-    }
 }
